79185c6f62bc7b4c116b6fe9c45be6c40abcbd97
Lincxx <bossco28@gmail.com>
2019-09-16 06:37:18 -0400

    Added in a new dir called BookList, Added in a Index page

---
+++ BookListRazor/Model/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+        //Create a DB set
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
